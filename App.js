223c557ca0295bb615a03b585547acb63c283ed9
Dylan Widjaja <dwidjaja945@gmail.com>
2018-09-17 12:30:13 -0700

    created initial stack navigation and screens it will direct to

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StackNavigator } from 'react-navigation';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+
+const AppStackNavigator = new StackNavigator({
+	WelcomeScreen: {
+		screen: WelcomeScreen
+	},
+	LoginScreen: {
+		screen: LoginScreen
+	},
+	SignpScreen: {
+		screen: SignUpScreen
+	}
+});
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello world</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<AppStackNavigator />
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
